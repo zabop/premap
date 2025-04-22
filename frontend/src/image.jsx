2f74180eaace2cb794a595a7c9705c19cb64a4e1
zabop <szabopal96@gmail.com>
2025-04-22 22:22:16 +0200

    post to deployed version

--- frontend/src/image.jsx
+++ frontend/src/image.jsx
@@ -23,8 +23,8 @@ export default function Image() {
     auth.xhr(
       {
         method: "POST",
-        //path: `https://premap.fly.dev/post`,
-        path: `http://0.0.0.0:8080/post`,
+        path: `https://premap.fly.dev/post`,
+        //path: `http://0.0.0.0:8080/post`,
         prefix: false,
         content: JSON.stringify({
           URL: imageUrl,
